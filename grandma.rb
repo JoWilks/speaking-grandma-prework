dc9db1aa1f36f9279d05f43c7ef0f46a29a9c25b
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-02 07:51:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,7 @@ def speak_to_grandma(phrase)
     "NO, NOT SINCE 1938!"
   elsif phrase == phrase.upcase
     "I LOVE YOU TOO PUMPKIN!"
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
